727e75b9d5b0b1b77a9e58675bf90a0ef7eb26e7
FacoChico <popov098097grigory@gmail.com>
2022-09-30 13:29:49 +0300

    Создал экран ввода логина и пароля, а также второй экран, на который будет перекидывать по нажатии кнопки при правильно введенных логине и пароле (посмотрите код и попробуйте запустить приложение)

--- app/src/main/java/com/example/finale/MainActivity.java
+++ app/src/main/java/com/example/finale/MainActivity.java
@@ -2,13 +2,48 @@ package com.example.finale;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button but1;
+    private EditText login, password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+        but1 = findViewById(R.id.button1);
+        login = findViewById(R.id.Login);
+        password = findViewById(R.id.Password);
+
+    but1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(login.getText().toString().trim().equals("DashaLohushka") && password.getText().toString().trim().equals("SashaTozhe")) {
+                    Toast.makeText(MainActivity.this, "Вы вошли в свой профиль", Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(MainActivity.this, MainSreen.class);
+                    startActivity(intent);
+                }
+                else if(login.getText().toString().trim().equals("")){
+                    Toast.makeText(MainActivity.this, "Введите логин", Toast.LENGTH_LONG).show();
+                }
+                else if(password.getText().toString().trim().equals("")){
+                    Toast.makeText(MainActivity.this, "Введите пароль", Toast.LENGTH_LONG).show();
+                }
+                else if(login.getText().toString().trim().equals("") && password.getText().toString().trim().equals("")){
+                    Toast.makeText(MainActivity.this, "Введите логин и пароль", Toast.LENGTH_LONG).show();
+                }
+                else if(!login.getText().toString().trim().equals("DashaLohushka") || !password.getText().toString().trim().equals("SashaTozhe")){
+                    Toast.makeText(MainActivity.this, "Данные не найдены", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
+
+
 }
