ff402cdbf51e234dfc00cc9f1d0e29119350ed11
makbuk <16yungsanches@gmail.com>
2022-10-16 22:40:11 +0300

    Во вкладке с кредитами добавлен массив, пока только имеющихся кредитов Чутка также изменен класс Credits.java

--- app/src/main/java/com/example/finale/Credits.java
+++ app/src/main/java/com/example/finale/Credits.java
@@ -10,24 +10,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.example.finale.databinding.ActivityCreditsBinding;
 
 public class Credits extends AppCompatActivity {
 
-    private ActivityCreditsBinding binding;
-
+    //private ActivityCreditsBinding binding;
+    String[] array = {"Кредит 1", "Кредит 2", "Кредит 3"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        setContentView(R.layout.activity_credits);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        CollapsingToolbarLayout toolBarLayout = findViewById(R.id.toolbar_layout);
+        toolBarLayout.setTitle(getTitle());
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_listview, array);
+
+        ListView listView = (ListView)findViewById(R.id.list);
+        listView.setAdapter(adapter);
+        /*
         binding = ActivityCreditsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
         Toolbar toolbar = binding.toolbar;
         setSupportActionBar(toolbar);
         CollapsingToolbarLayout toolBarLayout = binding.toolbarLayout;
         toolBarLayout.setTitle(getTitle());
-
+        */
     }
 }
